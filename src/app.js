0243f735c2fcd157f8a5783cabaf4a5c145e9172
Shivam Rajput <58025059+shiviraj@users.noreply.github.com>
2020-10-04 16:22:22 +0530

    modified github actions

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import path from 'path';
+import fs from 'fs';
+
+import express from 'express';
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+import App from '../src/App';
+
+const router = express.Router();
+
+router.use('^/$', (req, res, next) => {
+  fs.readFile(path.resolve('../public/index.html'), 'utf8', (err, data) => {
+    if (err) {
+      return res.status(500).send('An error occurred');
+    }
+    const app = ReactDOMServer.renderToString(<App />);
+    data.replace('<div id="root"></div>', `<div id="root">${app}</div>`);
+    res.send(data);
+  });
+});
+
+module.exports = router;
